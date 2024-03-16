73e64021cbef5dfbeabf5cce61145ce075816a71
Theaveas So <theaveasso@theaveasso.local>
2024-03-16 20:18:42 +0700

    update order handler

--- cmd/api/handler.order.go
+++ cmd/api/handler.order.go
@@ -9,3 +9,23 @@ func createOrderHandler(w http.ResponseWriter, r *http.Request) {
     body := map[string]interface{}{"message": "create order"}
     pkg.WriteJSON(w, http.StatusCreated, body)
 }
+
+func listOrdersHandler(w http.ResponseWriter, r *http.Request) {
+    body := map[string]interface{}{"message": "list orders"}
+    pkg.WriteJSON(w, http.StatusOK, body)
+}
+
+func getOrderHandler(w http.ResponseWriter, r *http.Request) {
+    body := map[string]interface{}{"message": "get order"}
+    pkg.WriteJSON(w, http.StatusOK, body)
+}
+
+func deleteOrderHandler(w http.ResponseWriter, r *http.Request) {
+    body := map[string]interface{}{"message": "delete order"}
+    pkg.WriteJSON(w, http.StatusOK, body)
+}
+
+func updateOrderHandler(w http.ResponseWriter, r *http.Request) {
+    body := map[string]interface{}{"message": "update order"}
+    pkg.WriteJSON(w, http.StatusOK, body)
+}
